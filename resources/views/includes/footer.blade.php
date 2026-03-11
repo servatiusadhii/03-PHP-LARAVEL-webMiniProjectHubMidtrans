9772f65e3634f087bd5a602571e67d93a4c67dd8
servatiusadhii <servatiusadhi13@gmail.com>
2026-03-11 09:02:25 +0700

    initial new commit

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,11 @@
+<footer>
+    <div class="container">
+    <div class="row">
+        <div class="col-12 text-center">
+        <p class="pt-4 pb-2">
+            2025 - <?= date('Y') ?> Copyright Mini Project Hub. All Rights Reserved S4N.
+        </p>
+        </div>
+    </div>
+    </div>
+</footer>
